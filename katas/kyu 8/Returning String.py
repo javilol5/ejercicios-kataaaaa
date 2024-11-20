39559466142170c6b38b64bb6c353c7b92cb2b0d
javilol5 <javierlolcasal@gmail.com>
2024-11-20 12:07:52 +0100

    feat: :bug: comente algunas lineas que habia de mas

--- katas/kyu 8/Returning String.py
+++ katas/kyu 8/Returning String.py
@@ -6,5 +6,5 @@ def greet(name):
     return f"Hello, {name} how are you doing today?"
 
 
-def greet(name):
-    return 'Hello, ' + name + ' how are you doing today?'
+#def greet(name):
+#    return 'Hello, ' + name + ' how are you doing today?'
