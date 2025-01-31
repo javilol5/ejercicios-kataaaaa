f6cfc9b7e5659b65f5b2dbd1261d3ce5d303a86a
javilol5 <javierlolcasal@gmail.com>
2025-01-31 11:21:19 +0100

    feat: :sparkles: modificacion a un kata de python erroneo

--- katas/python/kyu 8/You Can't Code Under Pressure #1.py
+++ katas/python/kyu 8/You Can't Code Under Pressure #1.py
@@ -1,10 +1,9 @@
 #https://www.codewars.com/kata/53ee5429ba190077850011d4
 
 def double_integer(i):
-    i = i*2
-    return(i)
-
-
-
-def double_integer(i):
     return(i*2)
+
+
+#def double_integer(i):
+#    i = i*2
+#    return(i)
